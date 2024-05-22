e5f6fb6a4a6361591cb539f93a306bcd90b495ef
takkun-robo <tkkn.robo@gmail.com>
2024-05-23 03:52:10 +0900

    ROSのsetup.bashを読み込むかどうか設定できるようにした
     Changes to be committed:
    	new file:   bash_etc/ros.bash
    	modified:   bashrc

---
+++ bash_etc/ros.bash
@@ -0,0 +1,57 @@
+
+# Switch whether to load the ros setup script when bash starts
+# マシンを再起動するたびにswitch_load_ros_setupを呼び出して読み込む設定にしないとrosのsetup.bashが読み込まれないようにしてる
+
+is_enable_load_ros_setup_script=false
+
+load_ros_setup_if_enable() {
+	local ros_setup_script_path=/opt/ros/iron 
+
+	case ${is_enable_load_ros_setup_script} in
+		true )
+		source ${ros_setup_script_path}/setup.bash
+		;;
+		false )
+		;;
+		* )
+		;;
+	esac
+}
+
+switch_load_ros_setup() {
+	local usage='usage: switch_load_ros_setup [yes|no|help]. help for more help.'
+
+	if [ $# -le 0 ];
+	then
+		echo ${usage}
+		return 1
+	elif test $# -ge 2;
+	then
+		echo ${usage}
+		return 1
+	fi
+
+	case $1 in
+		y|Y|yes|Yes )
+			is_enable_load_ros_setup_script=true
+			;;
+		n|N|no|No )
+			is_enable_load_ros_setup_script=false
+			;;
+		help)
+			cat - <<- 'EOF'
+			this function set enable/disable to load ros setup script.
+			acceptable agrs:
+			y|Y|yes|Yes : load if bashrc is loaded.
+			n|N|no|No   : not load.
+			help        : show this help.
+			EOF
+			;;
+		* )
+			echo ${usage}
+			return 1
+			;;
+	esac
+	
+}
+
--- bashrc
+++ bashrc
@@ -6,12 +6,23 @@ alias ll='ls -alF'
 alias la='ls -AF'
 alias l='ls -CF'
 
+alias gr='grep'
+
+alias ..='cd ../'
+alias ..2='cd ../../'
+
+alias t='touch'
+
 alias cp='cp -i'
 alias mv='mv -i'
 alias rm='rm -i'
 
 alias v='vim'
 alias vd='vimdiff'
+
+alias tm='tmux'
+alias tml='tmux ls'
+alias tma='tmux attach'
 
 alias g='git'
 alias ga='git add'
@@ -20,4 +31,10 @@ alias gd='git diff'
 alias gc='git commit'
 
 alias reload='source ~/.bashrc'
+
+# load ros setup script if you want
+if source ~/dotfiles/bash_etc/ros.bash;
+then
+	load_ros_setup_if_enable
+fi
 
